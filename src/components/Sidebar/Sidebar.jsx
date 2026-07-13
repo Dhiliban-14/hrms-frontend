79b68425ae8f0535733b7636ad61abd602c5fa47
DHILIBAN <dhiliban1408@gmail.com>
2026-07-13 12:37:30 +0530

    style: remove text company-name span next to logo completely

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -62,7 +62,6 @@ function Sidebar() {
 
       <div className="sidebar-logo">
         <img src={logo} alt="ZeAI Logo" />
-        <span className="company-name">Soft</span>
       </div>
 
       <nav className="sidebar-menu">
