96a3d2d806bf335de1a0d014c8a6dcc40c56d940
DHILIBAN <dhiliban1408@gmail.com>
2026-07-14 10:44:15 +0530

    style: replace hardcoded description text with dynamic selected language state inside Preferences

--- src/pages/Settings/Settings.jsx
+++ src/pages/Settings/Settings.jsx
@@ -284,7 +284,7 @@ function Settings() {
             <Globe size={22} />
             <div>
               <h4>Language</h4>
-              <p>Select display language</p>
+              <p>{language}</p>
             </div>
             <select 
               value={language}
